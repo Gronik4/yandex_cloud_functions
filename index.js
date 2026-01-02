9548675499af3f9085072d3791c535f1e84c8be4
Gronik4 <golegni@yandex.ru>
2026-01-02 18:37:36 +0300

    Add zip and README.md

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-import serverless from 'serverless-http';
+const serverless = require('serverless-http');
 const { characters } = require('./characters');
 
 const app = express();
@@ -16,6 +16,4 @@ app.get('/api/character/:id', (req, res) => {
   const idx = characters.findIndex(el=> el.id === id);
   res.json(characters[idx]);
 });
-
-//app.listen(process.env.PORT || 3000, () => {console.log('Север запущен на порту 3000.');})// ТОлько для проверки локально - удалить
-module.exports.hendler = serverless(app);
+module.exports.handler = serverless(app);
